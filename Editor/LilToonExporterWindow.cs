0a7cd03e7c0df4505154ed2f3e974ce716521083
mitsuya0077 <19956229+mitsuya0077@users.noreply.github.com>
2026-09-17 11:14:05 +0900

    Release 0.11.0: registered humanoid poses (#59)

--- Editor/LilToonExporterWindow.cs
+++ Editor/LilToonExporterWindow.cs
@@ -392,7 +392,7 @@ namespace VRVlog.LilToonExporter
         private static string PackageVersion()
         {
             var info = PackageManagerPackageInfo.FindForAssembly(typeof(LilToonExporterWindow).Assembly);
-            return info != null && !string.IsNullOrWhiteSpace(info.version) ? info.version : "0.10.6";
+            return info != null && !string.IsNullOrWhiteSpace(info.version) ? info.version : "0.11.0";
         }
 
         private static string InstalledLilToonStatus()
